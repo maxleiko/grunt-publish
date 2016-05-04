d4df6e441edd9203c66e18c53c50c7416dc7d499
Maxime Tricoire <max.tricoire@gmail.com>
2016-05-04 16:34:25 +0200

    v1.0.0: update all deps + add exitOnFailure option

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,57 +10,60 @@
 
 module.exports = function(grunt) {
 
-    // Project configuration.
-    grunt.initConfig({
-        jshint: {
-            all: [
-                'Gruntfile.js',
-                'tasks/*.js',
-                '<%= nodeunit.tests %>',
-            ],
-            options: {
-                jshintrc: '.jshintrc'
-            }
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>',
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp']
+    },
+
+    // Configuration to be run (and then tested).
+    publish: {
+      main: {
+        options: {
+          exitOnFailure: true
         },
+        src: [
+          'test/fixtures/aFolder/another-module',
+          'test/fixtures/fake-module'
+        ]
+      },
+      regex: {
+        src: ['test/fixtures/**/*']
+      }
+    },
 
-        // Before generating any new files, remove any previously-created files.
-        clean: {
-            tests: ['tmp']
-        },
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js']
+    }
 
-        // Configuration to be run (and then tested).
-        publish: {
-            main: {
-                src: [
-                    'test/fixtures/aFolder/another-module',
-                    'test/fixtures/fake-module'
-                ]
-            },
-            regex: {
-                src: ['test/fixtures/**/*']
-            }
-        },
+  });
 
-        // Unit tests.
-        nodeunit: {
-            tests: ['test/*_test.js']
-        }
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
 
-    });
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-    // Actually load this plugin's task(s).
-    grunt.loadTasks('tasks');
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'publish', 'nodeunit']);
 
-    // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
-    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-    // plugin's task(s), then test the result.
-    grunt.registerTask('test', ['clean', 'publish', 'nodeunit']);
-
-    // By default, lint and run all tests.
-    grunt.registerTask('default', ['jshint', 'test']);
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
 
 };
--- tasks/publish.js
+++ tasks/publish.js
@@ -7,92 +7,106 @@
  */
 
 'use strict';
-var path            = require('path'),
-    async           = require('async'),
-    npmPublisher    = require('./lib/npm-publisher');
+var path = require('path'),
+  async = require('async'),
+  npmPublisher = require('./lib/npm-publisher');
 
 module.exports = function(grunt) {
 
-    // Please see the Grunt documentation for more information regarding task
-    // creation: http://gruntjs.com/creating-tasks
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
 
-    grunt.registerMultiTask('publish', 'Automatically publish to NPM registry one or more modules', function() {
-        var done    = this.async(),
-            tasks   = [],
-            errors  = [],
-            success = [];
+  grunt.registerMultiTask('publish', 'Automatically publish to NPM registry one or more modules', function() {
+    var done = this.async(), tasks = [];
 
-        // Merge task-specific and/or target-specific options with these defaults.
-        var options = this.options({
-            ignore: ['node_modules'],
-            registry: null
-        });
-
-        /**
-         * Validates "ignore" option for given path
-         * @param filepath
-         * @returns {*}
-         */
-        function getIgnored(filepath) {
-            for (var i in options.ignore) {
-                if (filepath.indexOf(options.ignore[i]) !== -1) {
-                    return options.ignore[i];
-                }
-            }
-            return null;
-        }
-
-        /**
-         * Tells whether or not, the given path is a valid directory (directory which contains a package.json)
-         * @param filepath
-         * @returns {*}
-         */
-        function isValidDir(filepath) {
-            if (grunt.file.isDir(filepath)) {
-                return grunt.file.exists(path.resolve(filepath, 'package.json'));
-            }
-            return false;
-        }
-
-        // Iterate over all specified file groups.
-        this.files.forEach(function(f) {
-            f.src.forEach(function (filepath) {
-                var ignored = getIgnored(filepath);
-                if (!ignored && isValidDir(filepath)) {
-                    var moduleName = grunt.file.readJSON(path.resolve(filepath, 'package.json')).name;
-                    grunt.log.ok('Publishing ' + moduleName + ' (' + filepath + ') ...');
-                    tasks.push(function (cb) {
-                        npmPublisher(filepath, options, function (err) {
-                            if (err) {
-                                grunt.log.error('Unable to publish ' + moduleName + ' (' + err.message.split('\n')[0] + ')');
-                                errors.push(moduleName + ' (' + err.message.split('\n')[0] + ')');
-                                return cb();
-                            }
-                            grunt.log.ok(moduleName + ' published successfully');
-                            success.push(moduleName);
-                            cb();
-                        });
-                    });
-                }
-            });
-        });
-
-        async.parallel(tasks, function () {
-            var i;
-            if (success.length > 0) {
-                grunt.log.subhead('Successfully published:');
-                for (i in success) {
-                    grunt.log.ok(path.basename(success[i]));
-                }
-            }
-            if (errors.length > 0) {
-                grunt.log.subhead('Unable to publish:');
-                for (i in errors) {
-                    grunt.log.error(path.basename(errors[i]));
-                }
-            }
-            done();
-        });
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      ignore: ['node_modules'],
+      registry: null,
+      exitOnFailure: false
     });
+
+    /**
+     * Validates "ignore" option for given path
+     * @param filepath
+     * @returns {*}
+     */
+    function getIgnored(filepath) {
+      for (var i in options.ignore) {
+        if (filepath.indexOf(options.ignore[i]) !== -1) {
+          return options.ignore[i];
+        }
+      }
+      return null;
+    }
+
+    /**
+     * Tells whether or not, the given path is a valid directory (directory which contains a package.json)
+     * @param filepath
+     * @returns {*}
+     */
+    function isValidDir(filepath) {
+      if (grunt.file.isDir(filepath)) {
+        return grunt.file.exists(path.resolve(filepath, 'package.json'));
+      }
+      return false;
+    }
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      f.src.forEach(function(filepath) {
+        var ignored = getIgnored(filepath);
+        if (!ignored && isValidDir(filepath)) {
+          var moduleName = grunt.file.readJSON(path.resolve(filepath, 'package.json')).name;
+          tasks.push(function(cb) {
+            grunt.log.ok('Publishing ' + moduleName + ' (' + filepath + ') ...');
+            npmPublisher(filepath, options, function(err) {
+              if (err) {
+                if (options.exitOnFailure) {
+                  cb(err);
+                } else {
+                  cb(null, { name: moduleName, err: err });
+                }
+              } else {
+                grunt.log.ok(moduleName + ' published successfully');
+                cb(null, { name: moduleName });
+              }
+            });
+          });
+        }
+      });
+    });
+
+    async[options.exitOnFailure ? 'series':'parallel'](tasks, function(err, results) {
+      if (err) {
+        done(err);
+      } else {
+        var success = [], error = [];
+        for (var i in results) {
+          if (results[i].err) {
+            error.push(results[i]);
+          } else {
+            success.push(results[i]);
+          }
+        }
+
+        if (success.length > 0) {
+          grunt.log.subhead('Successfully published:');
+        }
+        success.forEach(function (item) {
+          grunt.log.ok(path.basename(item.name));
+        });
+
+        if (error.length > 0) {
+          grunt.log.subhead('Unable to publish:');
+        }
+        error.forEach(function (item) {
+          grunt.log.error(path.basename(item.err));
+        });
+
+        done();
+      }
+    });
+  });
 
 };
